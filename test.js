25a839e62253daecca0af117d1765ee0429044e8
Andrea Ceccato <andrea.ceccato@ululab.it>
2022-05-13 12:37:40 +0200

    Debug mode console.log e axiosObject

--- test.js
+++ test.js
@@ -1,4 +1,5 @@
 const Sap = require('./index');
+const fs = require('fs');
 // or
 // const Sap = require('./lib/sap');
 
@@ -7,10 +8,7 @@ async function run() {
    await sap.disconnect();
 }
 
-run();
-
-
-
+// run();
 
 
 
@@ -31,7 +29,7 @@ async function findAnagrafica(code) {
 
   var sap = await Sap.connect();
 
-  await sap.get('BusinessPartners', {});
+  await sap.get('BusinessPartners', {'$select': 'CardCode,FederalTaxID', $filter: `startswith(FederalTaxID, '${code}')`});
 
   console.log(sap);
 
@@ -39,11 +37,13 @@ async function findAnagrafica(code) {
 
   console.log(sap);
 
+  console.log( 'data.responseData' );
+
+  console.log(sap.responseData.value )
   return sap;
 }
 
-// console.log( findAnagrafica('code') );
-
+// findAnagrafica('IT0762963015');
 
 // await sap1.disconnect()
 // console.log(sap1);
@@ -68,3 +68,88 @@ async function findAnagrafica(code) {
 // sap.get('BusinessPartners', {})
 
 // var sapApi = await Sap.run('getBusinessPartners');
+
+async function insertPreventivo() {
+
+   var data = {
+     CardCode:  'C000082', // Codice cliente sap
+     U_MCR_STATOFF: '001',
+     DocDate: '2022-03-23',
+     DocumentLines: [
+         {
+             ItemCode: 'PREVENTIVO CRM',
+             Quantity: '1',
+             TaxCode: 'T1',
+             UnitPrice: '3434'
+         }
+     ]
+   };
+
+   var sap = await Sap.connect();
+   console.log(sap);
+   await sap.post('Quotations', data);
+
+   await sap.disconnect();
+
+   console.log(sap);
+
+   return sap;
+ }
+
+// insertPreventivo();
+
+let pdf = fs.readFileSync('test/file-preventivo.pdf').toString();
+
+console.log(pdf);
+
+async function invioDocumentoPreventivo() {
+
+  var sap = await Sap.connect();
+  sap.debug = true;
+  sap.setHeaders({'Content-Type': 'multipart/form-data;boundary=--webappPDF--'});
+
+  console.log(sap.getHeaders());
+  var data1 = [
+    '--webappPDF--\n\n',
+    'Content-Disposition: form-data; name="webapp_test_AndreaCeccatoUlulab_001";\n',
+    'filename="webapp_test_AndreaCeccatoUlulab_001.pdf"\n',
+    'Content-Type: application/pdf\n\n',
+    `${pdf}\n\n`,
+    '--webappPDF--\n',
+  ];
+
+  //console.log(data1.join(''));
+
+  await sap.post('Attachments2', data1.join(''));
+  /*var data2 = {
+    Attachments2_Lines: [
+      {
+        FileExtension: '.pdf',
+        FileName: 'webapp_test_AndreaCeccatoUlulab_001.pdf',
+        SourcePath: '/test',
+      }
+    ]
+  }*/
+  //await sap.post('Attachments2', data2);
+  //console.log(sap);
+
+  //await sap.patch(`Quotations(1173)`, {});
+  // console.log(sap.getHeaders())
+  await sap.disconnect();
+
+  console.log(sap);
+  return sap;
+
+}
+
+async function debug() {
+  var sap = await Sap.connect();
+  sap.debug = true;
+  console.log(sap);
+  await sap.disconnect();
+}
+
+//debug();
+//console.log(pdf);
+
+invioDocumentoPreventivo();
